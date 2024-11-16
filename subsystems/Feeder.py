2a3fc4eb5828cfbd3e71b0ac3ffff344448d545d
Tyler R Rindels <36929500+trindels@users.noreply.github.com>
2024-11-15 23:05:11 -0600

    Improved Feeder Logging and Simulation

--- subsystems/Feeder.py
+++ subsystems/Feeder.py
@@ -1,8 +1,10 @@
 from commands2 import Subsystem, Command
 
 from wpilib import RobotState, DigitalInput, SmartDashboard
+from wpimath.system.plant import DCMotor
+from wpimath.units import kSecondsPerMinute
 from ntcore import NetworkTable, NetworkTableInstance
-from rev import SparkMax, SparkRelativeEncoder, SparkRelativeEncoderSim
+from rev import SparkMax, SparkRelativeEncoder, SparkMaxSim, SparkRelativeEncoderSim
 
 class FeederModes:
     STOP:float = 0.0
@@ -13,6 +15,10 @@ class FeederModes:
     EJECT:float = -1.0
     MAX:float = 1.0
     MIN:float = -1.0
+
+class FeederConstants:
+    class NeoSim:
+        kMaxRpm = DCMotor.NEO(1).freeSpeed * kSecondsPerMinute
 
 class FeederBalance(Command):
     def __init__(self, intake:Subsystem):
@@ -47,6 +53,13 @@ class Feeder(Subsystem):
         # Motor Settings
         self.__motor = SparkMax( 22, SparkMax.MotorType.kBrushless )
         self.__motorEncoder:SparkRelativeEncoder = self.__motor.getEncoder()
+
+        # Simulation
+        self.__motorSim = SparkMaxSim( self.__motor, DCMotor.NEO(1) )
+        self.__motorSim.getAbsoluteEncoderSim().setPositionConversionFactor(1)
+        self.__motorSim.getAbsoluteEncoderSim().setVelocityConversionFactor(1)
+        self.__motorSim.getRelativeEncoderSim().setPositionConversionFactor(1)
+        self.__motorSim.getRelativeEncoderSim().setVelocityConversionFactor(1)
         
         # IR Break Beams
         self.__bottomIrBeam:DigitalInput = DigitalInput(1)
@@ -56,7 +69,8 @@ class Feeder(Subsystem):
         self.balanceCmd = FeederBalance(self)
 
         # Logging
-        self.__logging = NetworkTableInstance.getDefault().getTable("/Logging/Feeder")
+        self.__logger = NetworkTableInstance.getDefault().getTable("/Logging/Feeder")
+        self.__measured = NetworkTableInstance.getDefault().getTable("/RealOutputs/Feeder")
 
         # Dashboards
         SmartDashboard.putData( "Intake", self )
@@ -64,25 +78,36 @@ class Feeder(Subsystem):
     # Periodic Loop
     def periodic(self) -> None:
         # Logging: Write Current Subsystem State
-        self.__logging.putNumber( "aMotorPositionRot", self.__motorEncoder.getPosition() )
-        self.__logging.putNumber( "aMotorVelocityRPM", self.__motorEncoder.getVelocity() )
-        self.__logging.putBoolean( "bSensorTop", self.topHasNote() )
-        self.__logging.putBoolean( "bSensorBottom", self.bottomHasNote() )
+        self.__logger.putNumber( "MotorInput", self.__motor.get() )
+        self.__logger.putNumber( "MotorOutput", self.__motor.getAppliedOutput() )
+        self.__logger.putNumber( "MotorPosition_r", self.__motorEncoder.getPosition() )
+        self.__logger.putNumber( "MotorVelocity_rpm", self.__motorEncoder.getVelocity() )
+        self.__logger.putBoolean( "SensorTop", self.__topIrBeam.get() )
+        self.__logger.putBoolean( "SensorBottom", self.__bottomIrBeam.get() )
 
         # Run Subsystem: Set New State To Subsystem
         self.run()
       
         # Logging: Write Post Operation Information
-        self.__logging.putNumber( "Setpoint", self.getSetpoint() )
-        self.__logging.putNumber( "Measured", self.getMeasurement() )
-
+        cmdName = self.getCurrentCommand().getName() if self.getCurrentCommand() != None else "None"
+        self.__measured.putString( "Command", cmdName )
+        self.__measured.putNumber( "Setpoint", self.getSetpoint() )
+        self.__measured.putBoolean( "HasNote", self.hasSecuredNote() )
+        self.__measured.putBoolean( "HasNoteTop", self.topHasNote() )
+        self.__measured.putBoolean( "HasNoteBottom", self.bottomHasNote() )
+        
         # Misaligned Note Correction
         if self.getCurrentCommand() == None:
             if self.bottomHasNote() != self.topHasNote():
                 self.balanceCmd.schedule()
 
     def simulationPeriodic(self) -> None:
-        pass
+        # Motor Position and Velocity
+        driveRpm = FeederConstants.NeoSim.kMaxRpm * self.__motor.get()
+        self.__motorSim.getRelativeEncoderSim().iterate( driveRpm, 0.02 )
+        self.__motorSim.getRelativeEncoderSim().setVelocity( driveRpm )
+        self.__motorSim.getAbsoluteEncoderSim().setVelocity( driveRpm )
+        self.__motorSim.getAbsoluteEncoderSim().setPosition( self.__motorSim.getRelativeEncoderSim().getPosition() % 1 )
 
     # Run the Subsystem
     def run(self) -> None:
